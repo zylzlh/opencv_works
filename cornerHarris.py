3558d8db13cf8ca98eac9bc185026a4734ba8eeb
zyl <zylzlh0330@163.com>
2018-01-08 19:54:48 +0800

    changed cornerHarris.py

--- cornerHarris.py
+++ cornerHarris.py
@@ -6,6 +6,7 @@ This is a temporary script file.
 ""
 import cv2
 import numpy as np
+
 img=cv2.imread(r'C:\Users\zylzlh\Desktop\1.jpg')
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 dst=cv2.cornerHarris(gray,5,5,0.04)
