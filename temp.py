9d11e1c0d6c4de030c6d7d6411fc66df75de0f98
zyl <zylzlh0330@163.com>
2018-01-25 22:58:12 +0800

    person_detection

--- temp.py
+++ temp.py
@@ -6,27 +6,32 @@ This is a temporary script file.
 ""
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-img2=cv2.imread(r'C:\Users\zylzlh\Desktop\zhuanji.jpg',0)
-img1=cv2.imread(r'C:\Users\zylzlh\Desktop\biaozhi.jpg',0)
-sift=cv2.xfeatures2d.SIFT_create()
-kp1,des1=sift.detectAndCompute(img1,None)
-kp2,des2=sift.detectAndCompute(img2,None)
-FLANN_INDEX_KDTREE=0
-indexParams=dict(algorithm=FLANN_INDEX_KDTREE,tree=5)
-searchParams=dict(checks=50)
-flann=cv2.FlannBaesdMatcher(indexParams,searchParams)
-matches=flann.knnMatch(des1,des2,k=2)
-matchesMask=[[0,0] for i in range(len(matches))]
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i]=[1,0]
-drawParams=dict(matchColor=(0,255,0),
-                singlePointColor=(255,0,0),
-                matchesMask=matchesMask,
-                flags=0)
-resultImage=cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,
-                               None,**drawParams)
-plt.imshow(resultImage)
-plt.show()
+
+def is_inside(o,i):
+    ox,oy,ow,oh=o
+    ix,iy,iw,ih=i
+    return ox>ix and oy>iy and ox+ow<ix+iw and oy+oh<iy+ih
+
+def draw_person(image,person):
+    x,y,w,h=person
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+img=cv2.imread(r'C:\Users\zylzlh\Desktop\1.jpg',0)
+hog=cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+found,w=hog.detectMultiScale(img)
+found_filter=[]
+for ri,r in enumerate(found):
+    for qi,q in enumerate(found):
+        if ri!=qi and is_inside(r,q):
+            break
+        else:
+            found_filter.append(r)
+for person in found_filter:
+    draw_person(img,person)
+cv2.imshow("person detection",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
     
